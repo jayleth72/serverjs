ce7ebab6ae1a3eccba8154cfaaf6c0b02f6e86c1
jayleth72 <jay.letheby@gmail.com>
2017-01-16 11:27:03 +1000

    added projects page

--- server.js
+++ server.js
@@ -63,12 +63,16 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
 
   });
 });
 
-
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
 
 // /bad - send back with errorMessage
 app.get('/bad', (req, res) => {
